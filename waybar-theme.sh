4a5a7af2b2d31c68f4921da072115141a04deeeb
enrico223 <enrico.salamon@outlook.com>
2022-12-14 16:48:56 +0100

    refresh

--- waybar-theme.sh
+++ waybar-theme.sh
@@ -4,20 +4,20 @@ themes=(tokyonight powerline cotton\ factory)
 echo "Select a theme:"
 select theme in "${themes[*]}"; do
 	case $theme in
-		tokyonight)
+		"tokyonight")
 			cat /home/enrico/.config/waybar/tokyo-night.css > /home/enrico/.config/waybar/style.css
-			cat /home/enrico/.config/waybar/tokyo-night-config > /home/enrico/.config/waybar/config
+			cat /home/enrico/.config/waybar/tokyo-night-config > /home/enrico/.config/waybar/config;
+		break
+		"powerline"
+			cat /home/enrico/.config/waybar/powerline.css > /home/enrico/.config/waybar/style.css
+			cat /home/enrico/.config/waybar/powerline_config > /home/enrico/.config/waybar/config
+		break
+		"cotton factory"
+			cat /home/enrico/.config/waybar/cotton_factory.css > /home/enrico/.config/waybar/style.css
+			cat /home/enrico/.config/waybar/cotton_factory_config > /home/enrico/.config/waybar/config
+		break
+	esac
+done
+ 
 
-read selected_theme
 
-if [[$selected_theme -eq "tokyonight"]]
-then 
-	cat /home/enrico/.config/waybar/tokyo-night.css > /home/enrico/.config/waybar/style.css
-	cat /home/enrico/.config/waybar/tokyo-night-config > /home/enrico/.config/waybar/config
-elif [[$selected_theme -eq "powerline"]]
-	cat /home/enrico/.config/waybar/powerline.css > /home/enrico/.config/waybar/style.css
-	cat /home/enrico/.config/waybar/powerline_config > /home/enrico/.config/waybar/config
-elif [[$selected_theme -eq "cotton factory"]]
-	cat /home/enrico/.config/waybar/cotton_factory.css > /home/enrico/.config/waybar/style.css
-	cat /home/enrico/.config/waybar/cotton_factory_config > /home/enrico/.config/waybar/config
-fi
