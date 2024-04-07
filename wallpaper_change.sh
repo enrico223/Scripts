4d2857981b3dd50b4e7d474732cf20ff809a2a53
enrico <enrico.salamon@outlook.com>
2024-04-07 10:56:59 +0200

    minor changes

--- wallpaper_change.sh
+++ wallpaper_change.sh
@@ -7,6 +7,7 @@ hdmi="HDMI-A-1"
 laptop="eDP-1"
 wallpaper_dir="$HOME/Pictures/Wallpapers"
 time_stamp_file="$HOME/.last_hyprpaper_run"
+hyprpaper_conf="$HOME/.config/hypr/hyprpaper.conf"
 
 # Get list of wallpaper file paths
 file_listing=($wallpaper_dir/*)
@@ -14,16 +15,16 @@ file_listing=($wallpaper_dir/*)
 function hyprpaper_set() {
     # Get one random file path
     file_random=("${file_listing[RANDOM % ${#file_listing[@]}]}")
-	hyprctl hyprpaer wallpaper "HDMI-A-1, /home/enrico/Pictures/Wallpapers/cherry.webp"
+	#hyprctl hyprpaer wallpaper "HDMI-A-1, /home/enrico/Pictures/Wallpapers/cherry.webp"
 
     # Set wallpaper with hyprctl
-	if [[ $(hyprctl monitors | awk -F"[()]" '$2 && $2 ~ /-/ {print $2}') == $monitor ]]; then
+	if [[ $(hyprctl monitors | awk '/Monitor/ {print $2')]" '$2 && $2 ~ /-/ {print $2}') == $monitor ]]; then
 		hyprctl hyprpaper wallpaper "$monitor,$file_random"
-	elif [[ $(hyprctl monitors | awk -F"[()]" '$2 && $2 ~ /-/ {print $2}') == $monitor2 ]]; then
+	elif [[ $(hyprctl monitors | awk '/Monitor/ {print $2')]" '$2 && $2 ~ /-/ {print $2}') == $monitor2 ]]; then
 		hyprctl hyprpaper wallpaper "$monitor2,$file_random"
-	elif [[ $(hyprctl monitors | awk -F"[()]" '$2 && $2 ~ /-/ {print $2}') == $laptop ]]; then
+	elif [[ $(hyprctl monitors | awk '/Monitor/ {print $2')]" '$2 && $2 ~ /-/ {print $2}') == $laptop ]]; then
 		hyprctl hyprpaper wallpaper "$laptop,$file_random"
-	elif [[ $(hyprctl monitors | awk -F"[()]" '$2 && $2 ~ /-/ {print $2}') == $hdmi ]]; then
+	elif [[ $(hyprctl monitors | awk '/Monitor/ {print $2')]" '$2 && $2 ~ /-/ {print $2}') == $hdmi ]]; then
 		hyprctl hyprpaper wallpaper "$hdmi,$file_random"
 	else
 		notify-send "Something wrong happened"
@@ -43,6 +44,17 @@ function swaybg_set {
 		swaybg --image $file_random
 	fi
 }
+
+function update_hyprpaperconf {
+	sed -i '/preload/d' $hyprpaper_conf
+	for x in $(ls $wallpaper_dir);
+	do
+		echo preload = $wallpaper_dir/$x >> $hyprpaper_conf
+	done
+}
+
+update_hyprpaperconf
+
 # Check if the script was run less than a second ago
 if [ -f "$time_stamp_file" ]; then
     last_run=$(cat "$time_stamp_file")
@@ -57,7 +69,6 @@ fi
 
 # Save the current timestamp
 date +%s > "$time_stamp_file"
-
 # Call the function
 if [ $DESKTOP_SESSION == hyprland ]; then
 	hyprpaper_set
